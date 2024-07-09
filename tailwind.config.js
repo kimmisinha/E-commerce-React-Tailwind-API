95a0f829c060209a91df9d8b9581244dbfb97342
kimmisinha <kimmi.sinha@newput.com>
2024-07-09 15:27:09 +0530

    Implement product details view with addToCart functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,9 +21,7 @@ module.exports = {
         primary: '#222222',
         secondary: '#F5E6E0',
       },
-      backgroundImage: {
-        hero: "url('./img/bg_hero.svg')",
-      },
+     
     },
   },
   plugins: [],
